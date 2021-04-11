478b42c0ef1961d4312e716fad18e8b7c0f5e9bf
Sasha9805 <pimenovs33@gmail.com>
2021-04-11 16:01:21 +0300

    Добавлена библиотека полифилов core-js

--- build/main.js
+++ build/main.js
@@ -1,15 +1,43 @@
-"use strict";
+import "regenerator-runtime/runtime.js";
+import "core-js/modules/es.array.flat.js";
+import "core-js/modules/es.function.name.js";
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.promise.js";
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-class App {
-  constructor() {
-    _defineProperty(this, "run", async (name = 'World') => {
-      console.log(`Hello ${name}`);
-    });
-  }
+var App = function App() {
+  _classCallCheck(this, App);
 
-}
+  _defineProperty(this, "run", /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var name,
+        _args = arguments;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            name = _args.length > 0 && _args[0] !== undefined ? _args[0] : 'World';
+            console.log("Hello ".concat(name));
+            console.log([1, 2, [2, 3]].flat());
 
-const app = new App();
-app.run().then(() => console.log('done')).catch(err => console.log(err.name));
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  })));
+};
+
+var app = new App();
+app.run().then(function () {
+  return console.log('done');
+}).catch(function (err) {
+  return console.log(err.name);
+});
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
 
   run = async (name = 'World') => {
     console.log(`Hello ${name}`);
+    console.log([1,2,[2,3]].flat());
   };
 
 }
